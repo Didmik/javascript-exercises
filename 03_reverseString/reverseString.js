5d9a06f6d5a7509f844a936f54310c3aeb3b144c
Didrik Mikkelsen <didrikmikkelsen.92@gmail.com>
2023-05-01 15:51:12 +0200

    Finished task 3 in javascript exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(word) {
+   /* const wordArray = word.split("");
+    //debugging console.log(wordArray);
+    let wordBox = [];
+    for (let i = 0; i <= wordArray.length; i++) {
+        wordBox = wordBox + wordArray.pop();  
+         
+        //wordBox.unshift(deleteLast);
+    }
+    return wordBox; */
+    return word.split("").reverse().join("");
 
 };
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
