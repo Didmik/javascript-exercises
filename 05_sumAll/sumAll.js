16f048af10070355d775ec5e1efb4ce9d06752ce
Didrik Mikkelsen <didrikmikkelsen.92@gmail.com>
2023-05-02 11:15:44 +0200

    Finsihed task 5 in javascript exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num, num2) {
+   let sum = 0;
+   /*if (isNaN(num) || isNaN(num2)) {
+    return "ERROR";
+   } */
+   /*if (typeof num == "number" && typeof num2 == "number") */
+   if (!Number.isInteger(num) || !Number.isInteger(num2)) return "ERROR";
+    if (num < 0 || num2 < 0) {
+    return "ERROR";
+   }
+   else if (num > num2) {
+    for (let i = num2; i <= num; i++) {
+        sum += i;
+    }
+   }
+
+   for (let i = num; i <= num2; i++) {
+    sum += i;
+   }
+    
+
+   console.log(sum);
+   return sum;
+   
 
 };
+
+sumAll(2, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
