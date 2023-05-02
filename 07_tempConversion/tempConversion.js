e70b11cae1e9c78a4a28142bd93d60056753ffd5
Didrik Mikkelsen <didrikmikkelsen.92@gmail.com>
2023-05-02 19:41:24 +0200

    Finished task 7 in javascript exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let celCon = (fahr - 32) * 5/9;
+  return Math.round(celCon * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+convertToCelsius(45);
+
+const convertToFahrenheit = function(cel) {
+  let fahrCon = (cel * 9/5) + 32;
+  return Math.round(fahrCon * 10) / 10;
 };
+
+convertToFahrenheit(20);
 
 // Do not edit below this line
 module.exports = {
