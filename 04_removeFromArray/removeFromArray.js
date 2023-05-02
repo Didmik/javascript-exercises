a79384456f78ea79a261a9fd2eefb1e728a66aac
Didrik Mikkelsen <didrikmikkelsen.92@gmail.com>
2023-05-02 10:09:14 +0200

    Finished task 4 in javascript exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,39 @@
-const removeFromArray = function() {
+const removeFromArray = function(box, ...args) {
+  /*if (box.includes(num)) {
+    box.splice(num - 1, 1);
+  }
+  else if (box.includes(...args)) { 
+    box.splice(...args, 1);
+     }
+    
+  return box; */
+
+  /*
+  box.filter(newBox);
+
+  function newBox(item) {
+    if (!args.includes(item)) {
+        box.push(item);
+        return box;
+    }
+
+    }
+
+    */
+
+    return box.filter(item => !args.includes(item));
+
+    
+  
+
+
+/*for (const numb of box) {
+    if (num)
+} */
 
 };
+
+removeFromArray([1,2,3,4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
